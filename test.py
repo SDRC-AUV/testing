ba676daac9165294417612705fdd7973313bb6fb
Corey McNeish <cmcneish@asu.edu>
2016-07-16 19:23:26 -0700

    Updated RCIO channel test to read keyboard input

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import signal
 import sys
 from rawmovement import *
 
-def test_pitch(rm, power = 0.5):
+def test_pitch(rm, p = 0.5):
     #pitch up?
-    power = RCIOPowerNormalized(pitch = power)
+    power = RCIOPowerNormalized(pitch = p)
     rm.set_motor_power(power)
     time.sleep(0.5)
 
@@ -15,7 +15,7 @@ def test_pitch(rm, power = 0.5):
     time.sleep(0.5)
 
     #pitch down
-    power.set(pitch = -1*power)
+    power.set(pitch = -1*p)
     rm.set_motor_power(power)
     time.sleep(0.5)
 
@@ -23,9 +23,9 @@ def test_pitch(rm, power = 0.5):
     power.set(pitch = 0)
     rm.set_motor_power(power)
 
-def test_roll(rm, power = 0.5):
+def test_roll(rm, p = 0.5):
     #roll right?
-    power = RCIOPowerNormalized(roll = power)
+    power = RCIOPowerNormalized(roll = p)
     rm.set_motor_power(power)
     time.sleep(0.5)
 
@@ -35,7 +35,7 @@ def test_roll(rm, power = 0.5):
     time.sleep(0.5)
 
     #roll left?
-    power.set(roll = -1*power)
+    power.set(roll = -1*p)
     rm.set_motor_power(power)
     time.sleep(0.5)
 
@@ -43,9 +43,9 @@ def test_roll(rm, power = 0.5):
     power.set(roll = 0)
     rm.set_motor_power(power)
     
-def test_yaw(rm, power = 0.5):
+def test_yaw(rm, p = 0.5):
     #yaw right?
-    power = RCIOPowerNormalized(yaw = power)
+    power = RCIOPowerNormalized(yaw = p)
     rm.set_motor_power(power)
     time.sleep(0.5)
 
@@ -55,7 +55,7 @@ def test_yaw(rm, power = 0.5):
     time.sleep(0.5)
 
     #yaw left?
-    power.set(yaw = -1*power)
+    power.set(yaw = -1*p)
     rm.set_motor_power(power)
     time.sleep(0.5)
 
@@ -64,9 +64,9 @@ def test_yaw(rm, power = 0.5):
     rm.set_motor_power(power)
     
 
-def test_forward(rm, power = 0.5):
+def test_forward(rm, p = 0.5):
     #forward
-    power = RCIOPowerNormalized(forward = power)
+    power = RCIOPowerNormalized(forward = p)
     rm.set_motor_power(power)
     time.sleep(0.5)
 
@@ -76,7 +76,7 @@ def test_forward(rm, power = 0.5):
     time.sleep(0.5)
 
     #backward
-    power.set(forward = -1*power)
+    power.set(forward = -1*p)
     rm.set_motor_power(power)
     time.sleep(0.5)
 
@@ -85,9 +85,9 @@ def test_forward(rm, power = 0.5):
     rm.set_motor_power(power)
     
 
-def test_lateral(rm, power = 0.5):
+def test_lateral(rm, p = 0.5):
     #strafe right?
-    power = RCIOPowerNormalized(lateral = 0.5)
+    power = RCIOPowerNormalized(lateral = p)
     rm.set_motor_power(power)
     time.sleep(0.5)
 
@@ -97,7 +97,7 @@ def test_lateral(rm, power = 0.5):
     time.sleep(0.5)
 
     #strafe left?
-    power.set(lateral = -0.5)
+    power.set(lateral = -1*p)
     rm.set_motor_power(power)
     time.sleep(0.5)
 
@@ -109,6 +109,19 @@ def signal_handler(signal, frame):
     disarm()
     sys.exit(0)
 
+
+def getch():
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
 signal.signal(signal.SIGINT, signal_handler)
 
 rospy.init_node('sdrcauv_main')
@@ -116,6 +129,18 @@ mavros.set_namespace()
 
 rm = RawMovement()
 
+
+zero_power = RCIOPowerNormalized()
+
 arm()
-test_forward(rm, 0.1)
+
+for i in range(5):
+    ch = getch()
+    channels = [0, 0, 0, 0, 0, 0]
+    channels[int(ch)-1] = 0.1
+
+    power = RCIOPowerNormalized(*channels)
+    rm.set_motor_power(power)
+    rospy.sleep(3)
+    rm.set_motor_power(zero_power)
 disarm()
